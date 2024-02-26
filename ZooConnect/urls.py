a97624d0c367a468d36be459b50e130cbe98dd4b
vincentyelpen <vincent.yelpen@outlook.com>
2024-02-26 21:22:35 +0100

    change the view logic to indivdual add ,delete,... and create templates from html

--- ZooConnect/urls.py
+++ ZooConnect/urls.py
@@ -1,24 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path
-from DigitalZoo.views import add_species, edit_species, search_species, delete_species, add_venue, edit_venue, search_venue, delete_venue, add_animal, edit_animal, search_animal, delete_animal
-
+from DigitalZoo.views import homepage_view,login_view,add_entity, edit_entity, search_entity, delete_entity
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
+    path('add_entity/', add_entity, name='add'),
+    path('edit_entity/', edit_entity, name='edit'),
+    path('search_entity/', search_entity, name='search'),
+    path('delete_entity/', delete_entity, name='delete_entity'),
+    path('homepage/', homepage_view, name='homepage'),
 
-    path('add_species/', add_species, name='add_species'),
-    path('edit_species/<int:species_id>/', edit_species, name='edit_species'),
-    path('search_species/', search_species, name='search_species'),
-    path('delete_species/<int:species_id>/', delete_species, name='delete_species'),
-
-    path('add_venue/', add_venue, name='add_venue'),
-    path('edit_venue/<int:venue_id>/', edit_venue, name='edit_venue'),
-    path('search_venue/', search_venue, name='search_venue'),
-    path('delete_venue/<int:venue_id>/', delete_venue, name='delete_venue'),
-
-    path('add_animal/', add_animal, name='add_animal'),
-    path('edit_animal/<int:animal_id>/', edit_animal, name='edit_animal'),
-    path('search_animal/', search_animal, name='search_animal'),
-    path('delete_animal/<int:animal_id>/', delete_animal, name='delete_animal'),
-]
+    ]
